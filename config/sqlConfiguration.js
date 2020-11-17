419d522040585ac58247b74f1535b1c57d8d1809
Kang Ho Dong <stardustsw8947@gmail.com>
2020-11-17 21:27:56 +0900

    Update sqlConfiguration.js

--- config/sqlConfiguration.js
+++ config/sqlConfiguration.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');  //My-sql을 사용하였다.
  
 const dbConn = mysql.createConnection({  //커넥션 생성
-  host: '49.50.163.131',
-  user: 'se_deu',
-  database: 'deu_se',
-  password : 'se_deu'
+  host: '',
+  user: '',
+  database: '',
+  password : ''
 });
 dbConn.connect();
 
